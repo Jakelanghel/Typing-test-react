b5e1e571dc13c08c813438fbd8019a689b8bd6de
Jake <jacobtlanghel@gmail.com>
2022-04-25 22:11:00 -0400

    moved logic into custom hook

--- src/App.js
+++ src/App.js
@@ -1,66 +1,10 @@
-import userEvent from "@testing-library/user-event";
-import React, { useState, useEffect, useRef } from "react";
 import TypingTest from "./components/TypingTest";
+import useTypingTest from "./hooks/useTypingTest"
 
 function App() {
-  const START_TIME = 2;
 
-  const [testData, setTestData] = useState({
-    timeRemaining: START_TIME,
-    isTimerRunning: false,
-    usrInput: "",
-    wordCount: 0,
-  });
-
-  const textBoxRef = useRef(null);
-
-  const startGame = () => {
-    setTestData((prevState) => {
-      return {
-        timeRemaining: START_TIME,
-        isTimerRunning: true,
-        usrInput: "",
-      };
-    });
-    textBoxRef.current.disabled = false;
-    textBoxRef.current.focus();
-  };
-
-  const endGame = () => {
-    const count = getWordCount()
-    setTestData(prevState => {
-      return ({
-        timeRemaining: START_TIME,
-        isTimerRunning: false,
-        usrInput: "",
-        wordCount: count,
-      })
-    })
-  }
-
-  const getWordCount = () => {
-    const wordsArr = testData.usrInput.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  };
-
-  const getInput = (e) => {
-    const input = e.target.value;
-    setTestData(prevState => ({...prevState, usrInput: input}))
-  };
-
-  useEffect(() => {
-    if (testData.isTimerRunning && testData.timeRemaining > 0) {
-      setTimeout(() => {
-        setTestData((prevState) => ({
-          ...prevState,
-          timeRemaining: testData.timeRemaining - 1,
-        }));
-      }, 1000);
-    }else if(testData.timeRemaining === 0) {
-      endGame()
-    }
-  }, [testData.timeRemaining, testData.isTimerRunning]);
-
+  const {testData, textBoxRef, startGame, getInput} = useTypingTest()
+  
   return (
     <div>
       <TypingTest
